55e7ec5f6fb9c206eeac2e4815b5c9d6585b216c
ManhLV <lemanhddt@gmail.com>
2024-06-19 01:05:14 +0700

    wrap request handler errors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { config } from 'dotenv'
 import databaseService from './services/database.service'
 import authRouter from './routes/auth.routes'
@@ -18,6 +18,11 @@ app.use(express.json())
 // Routes
 app.use('users', usersRouter)
 app.use('/auth', authRouter)
+
+// Error handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err?.message })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { registerController } from '~/controllers/auth.controllers'
 import { registerValidator } from '~/middlewares/auth/register-validator.middleware'
+import { wrapRequestHandler } from '~/middlewares/wrapRequestHandler'
 
 const authRouter = Router()
 
-authRouter.post('/register', registerValidator, registerController)
+authRouter.post('/register', registerValidator, wrapRequestHandler(registerController))
 
 export default authRouter
