ff69868a88652d65b9dd74a3503a833b0b618f57
ManhLV <lemanhddt@gmail.com>
2024-06-24 02:19:08 +0700

    filter upload

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,23 +1,14 @@
 import path from 'path'
 import { Response } from 'express'
 import { RequestData } from '~/constants/interfaces'
+import mediasServices from '~/services/medias.services'
+import { HttpStatus } from '~/constants/httpStatus'
 
 export const uploadSingleImageController = async (req: RequestData<any>, res: Response) => {
-  const formidable = (await import('formidable')).default
+  const data = await mediasServices.uploadSingleImage(req)
 
-  const form = formidable({
-    uploadDir: path.resolve('uploads'),
-    maxFiles: 1,
-    keepExtensions: true,
-    maxFileSize: 300 * 1024 // 300KB
-  })
-
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      throw err
-    }
-    res.json({
-      message: 'Upload image successfully'
-    })
+  return res.status(HttpStatus.OK).json({
+    message: 'Image uploaded successfully',
+    data
   })
 }
