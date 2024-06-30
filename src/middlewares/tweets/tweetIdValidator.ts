dc9e2d5322b2d722807febc26a289fd026b0a7d2
ManhLV <lemanhddt@gmail.com>
2024-07-01 02:37:52 +0700

    validate tweetid

---
+++ src/middlewares/tweets/tweetIdValidator.ts
@@ -0,0 +1,37 @@
+import { checkSchema } from 'express-validator'
+import { ObjectId } from 'mongodb'
+import { HttpStatus } from '~/constants/httpStatus'
+import { TWEETS_MESSAGES } from '~/constants/message'
+import { ErrorWithStatus } from '~/models/Error'
+import databaseService from '~/services/database.service'
+import { validate } from '~/utils/validate'
+
+export const tweetIdValidator = validate(
+  checkSchema(
+    {
+      tweet_id: {
+        custom: {
+          options: async (value, { req }) => {
+            if (!ObjectId.isValid(value)) {
+              throw new ErrorWithStatus({
+                status: HttpStatus.BAD_REQUEST,
+                message: TWEETS_MESSAGES.INVALID_TWEET_ID
+              })
+            }
+            const tweet = await databaseService.tweets.findOne({
+              _id: new ObjectId(value)
+            })
+            if (!tweet) {
+              throw new ErrorWithStatus({
+                status: HttpStatus.NOT_FOUND,
+                message: TWEETS_MESSAGES.TWEET_NOT_FOUND
+              })
+            }
+            return true
+          }
+        }
+      }
+    },
+    ['params', 'body']
+  )
+)
--- src/routes/bookmarks.routes.ts
+++ src/routes/bookmarks.routes.ts
@@ -2,16 +2,24 @@ import { Router } from 'express'
 import { bookmarkTweetController, unBookmarkTweetController } from '~/controllers/bookmarks.controllers'
 import { accessTokenValidator } from '~/middlewares/auth/accessTokenValidator'
 import { wrapRequestHandler } from '~/middlewares/errors/wrapRequestHandler'
+import { tweetIdValidator } from '~/middlewares/tweets/tweetIdValidator'
 import { verifiedUserValidator } from '~/middlewares/users/verifiedUserValidator'
 
 const bookmarksRouter = Router()
 
-bookmarksRouter.post('', accessTokenValidator, verifiedUserValidator, wrapRequestHandler(bookmarkTweetController))
+bookmarksRouter.post(
+  '',
+  accessTokenValidator,
+  verifiedUserValidator,
+  tweetIdValidator,
+  wrapRequestHandler(bookmarkTweetController)
+)
 
 bookmarksRouter.delete(
   '/tweets/:tweet_id',
   accessTokenValidator,
   verifiedUserValidator,
+  tweetIdValidator,
   wrapRequestHandler(unBookmarkTweetController)
 )
 
